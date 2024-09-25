bd148cb38538ce0fb97adf02d208c56a031d9463
Iiro-IT <iiro.a108301@student.winnova.fi>
2024-09-25 11:20:06 +0300

    css update

--- public_html/Projekti/resources/views/about.blade.php
+++ public_html/Projekti/resources/views/about.blade.php
@@ -1,38 +1,17 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __(" Socials") }}
+            {{ __('Socials') }}
         </h2>
     </x-slot>
 
-    <body class="flex items-center justify-center min-h-screen bg-gray-100">
-
-        <div class="text-center">
-            <!-- Flex container for horizontally aligning main texts -->
-            <div class="flex items-center justify-center space-x-8 mb-8">
-                <h1 class="text-3xl font-bold text-gray-100">Fisujärvi</h1>
-                <h1 class="text-3xl font-bold text-gray-100">4kur</h1>
-            </div>
-    
-            <!-- Grid for Undertext -->
-            <div class="grid grid-cols-2 gap-10">
-                <!-- First Column -->
-                <div>
-                    <ul class="space-y-2">
-                        <li class="text-gray-100">Undertext 1</li>
-                        <li class="text-gray-100">Undertext 2</li>
-                        <li class="text-gray-100">Undertext 3</li>
-                    </ul>
-                </div>
-    
-                <!-- Second Column -->
-                <div>
-                    <ul class="space-y-2">
-                        <li class="text-gray-100">Undertext 4</li>
-                        <li class="text-gray-100">Undertext 5</li>
-                        <li class="text-gray-100">Undertext 6</li>
-                    </ul>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
+    </div>
 </x-app-layout>
--- public_html/Projekti/resources/views/layouts/navigation.blade.php
+++ public_html/Projekti/resources/views/layouts/navigation.blade.php
@@ -12,10 +12,10 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link class="topDash" :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                    <x-nav-link class="topAbout" :href="route('about')" :active="request()->routeIs('about') ">
                         {{ __('about') }}
                     </x-nav-link>
                 </div>
