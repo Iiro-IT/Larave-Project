83dcbedc1927bd35feb7c90cd4b74f96d88f7fbc
Iiro-IT <iiro.a108301@student.winnova.fi>
2024-09-25 15:19:49 +0300

    avatar

--- public_html/Projekti/resources/views/about.blade.php
+++ public_html/Projekti/resources/views/about.blade.php
@@ -14,10 +14,18 @@
                         <div class="fisuCard">
                             <img src="fisu_avatar.png">
 
-
                             <div class="fisuContainer">
                               <h4><b>Fisujärvi</b></h4>
                               <p>SpriteMaker</p>
+
+                              <div class="4kurCard">
+                                <img src="4kur_avatar.jpg">
+    
+                                <div class="4kurContainer">
+                                  <h4><b>4kur</b></h4>
+                                  <p>Main programmer</p>
+                              </div> 
+                            </div>
                           </div> 
                         </div>
                     </div>
