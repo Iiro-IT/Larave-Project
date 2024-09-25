548fb161165a7063c8030a85a6343f2f592e3de2
Iiro-IT <iiro.a108301@student.winnova.fi>
2024-09-25 13:12:25 +0300

    parannuksia

--- public_html/Projekti/resources/views/about.blade.php
+++ public_html/Projekti/resources/views/about.blade.php
@@ -6,12 +6,14 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="container">
+            <div class="box">
+                <div class="content">
+                    <div class="message">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
