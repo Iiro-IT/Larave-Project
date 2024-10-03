0102a45f7bb321de9da613ff3775bf13e95d26f5
Iiro-IT <iiro.a108301@student.winnnova.fi>
2024-10-03 13:35:12 +0300

    muutoksia

--- public_html/Projekti/resources/views/4kur.blade.php
+++ public_html/Projekti/resources/views/4kur.blade.php
@@ -1,11 +1,21 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Socials') }}
+            {{ __('4kur Page') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-       
+        <div class="container">
+            <div class="box">
+                <div class="content">
+                    <div class="message">
+                        <div class="Akur-box">
+                            <textarea placeholder="Write your text here..."></textarea>
+                        </div>
+                    </div>
+                </div>
             </div>
-</x-app-layout> 
+        </div>
+    </div>
+</x-app-layout>
