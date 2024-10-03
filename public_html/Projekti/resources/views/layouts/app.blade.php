45c64d9234f6a541142cb08317862fa1c8e04614
Iiro-IT <iiro.a108301@student.winnnova.fi>
2024-10-03 10:31:00 +0300

    jo

--- public_html/Projekti/resources/views/layouts/app.blade.php
+++ public_html/Projekti/resources/views/layouts/app.blade.php
@@ -14,13 +14,13 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased bg-black">
-        <div class="min-h-screen bg-black-100 dark:bg-gray-900">
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="mainHeader">
+                <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
@@ -29,7 +29,6 @@
 
             <!-- Page Content -->
             <main>
-                
                 {{ $slot }}
             </main>
         </div>
--- public_html/Projekti/routes/web.php
+++ public_html/Projekti/routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,10 +10,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about'); 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
