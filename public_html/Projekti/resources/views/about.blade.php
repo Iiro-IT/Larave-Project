e8360c2e659b358a7b76dda0690d8cbd9daf7ff6
Iiro-IT <iiro.a108301@student.winnnova.fi>
2024-10-10 10:29:33 +0300

    pokemon Kortit

--- public_html/Projekti/resources/views/about.blade.php
+++ public_html/Projekti/resources/views/about.blade.php
@@ -1,33 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Socials') }}
+            {{ __('Pokémon Style Info Cards') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 taustakuva">
         <div class="container">
             <div class="laatikko">
                 <div class="content">
                     <div class="message">
 
-                        <div class="fisuCard">
+                        <!-- Fisu Pokémon Card -->
+                        <div class="pokemonCard fisuCard">
                             <a href="{{ url('/fisu') }}">
-                                <img src="fisu_avatar.png" alt="Fisu Avatar">
+                                <img src="fisu_avatar.png" alt="Fisu Avatar" class="fisuAvatar">
                             </a>
-            </div>
-
+                            <h3>Fisu</h3>
+                            <p class="description">Water-type Pokémon</p>
                         </div>
 
-                            <div class="AkurCard">
+                        <!-- Akur Pokémon Card -->
+                        <div class="pokemonCard AkurCard">
                             <a href="{{ url('/4kur') }}">
-                                <img src="Akur_avatar.jpg" alt="Akur Avatar">
+                                <img src="Akur_avatar.jpg" alt="Akur Avatar" class="akurAvatar">
                             </a>
-
-                                </div> 
-                            </div>
-
+                            <h3>Akur</h3>
+                            <p class="description">Fire-type Pokémon</p>
                         </div>
+
                     </div>
                 </div>
             </div>
--- public_html/Projekti/resources/views/layouts/app.blade.php
+++ public_html/Projekti/resources/views/layouts/app.blade.php
@@ -20,8 +20,8 @@
 
             <!-- Page Heading -->
             @isset($header)
-            <header class="mainHeader">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="mainHeader">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
