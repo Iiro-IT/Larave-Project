9cc4d06098bba921e2a2f3c42344d25f7483c8a6
Iiro-IT <iiro.a108301@student.winnnova.fi>
2024-10-17 14:41:12 +0300

    JOO

--- public_html/Projekti/resources/views/products/product_create.blade.php
+++ public_html/Projekti/resources/views/products/product_create.blade.php
@@ -9,7 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    
+                    form action="{{ route('ProductController', ['id'=>1]) }}">
+                    Tuotteen nimi <input type="text" name="price" id="price">
+                    @error("price")
+                        <div class="alert alert-danger">{{ $message }} </div>
+
                 </div>
             </div>
         </div>
