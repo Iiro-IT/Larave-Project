a48efcc8c910728680d3195f8ba0ae461c213630
Iiro-IT <iiro.a108301@student.winnnova.fi>
2024-11-21 14:58:46 +0200

    omien sivujen päivitys

--- public_html/Projekti/resources/views/fisu.blade.php
+++ public_html/Projekti/resources/views/fisu.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         </h2>
@@ -9,12 +13,33 @@
             <div class="Avatarlaatikko">
                 <div class="Avatarcontent">
                     <div class="Avatarmessage">
-                        <div class="fisuBox">
-                            <textarea id="dynamicFisuTextarea" readonly placeholder="Check our Instagram:">
-                                Fisu Text.......</textarea>
-                        </div>
+                        <div class="Osa1">
+                            <div class="fisuBox">
+                                <textarea id="dynamicFisuTextarea" readonly placeholder="Check our Instagram:">
+                                    Fisu Text.......</textarea>
+                            </div>
+                        <img class="Avatar" src="fisu_avatar.png">
                     </div>
-                    <img class="Avatar" src="fisu_avatar.png">
+                    <div>
+                        <footer>
+                            <div class="top-footer">
+                                <p>Fisujärvi</p>
+                            </div>
+                            <div class="middle-footer">
+                                <p>
+                                <div class ="CopyRight_footer">
+                                    <br>Copyright &copy; Fisujärvi 2024 - 2025<br>
+                                </div>
+                                </p>
+                            </div>
+                            <div class="social_icons_footer">
+                                <div class="icon"><i class="fa-brands fa-x-twitter"></i></div>
+                                <div class="icon"><i class="fa-brands fa-twitch"></i></div>
+                                <div class="icon"><i class="fa-brands fa-youtube"></i></div>
+                                <div class="icon"><i class="fa-brands fa-github"></i></div>
+                            </div>
+                        </footer>
+                    </div>
                 </div>
             </div>
         </div>
