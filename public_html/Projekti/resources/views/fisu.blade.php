66d49c291ec2bea5a243e62dcc21d5291b2ecb8a
Iiro-IT <iiro.a108301@student.winnnova.fi>
2024-11-28 14:40:04 +0200

    valmis

--- public_html/Projekti/resources/views/fisu.blade.php
+++ public_html/Projekti/resources/views/fisu.blade.php
@@ -2,7 +2,7 @@
 
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         </h2>
@@ -16,41 +16,56 @@
                         <div class="Osa1">
                             <div class="fisuBox">
                                 <textarea id="dynamicFisuTextarea" readonly placeholder="Check our Instagram:">
-                                    Fisu Text.......</textarea>
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+kerra heitettii päärynä vitu lujaa päin seinää (juuso heitti ja se käy salilla) ja kuulu vaan poks eikä jääny mössöö tai mtn missää vaa se hävis kokonaan.
+                                </textarea>
                             </div>
-                        <img class="Avatar" src="fisu_avatar.png">
-                    </div>
-                    <div>
-                        <footer>
-                            <div class="top-footer">
-                                <p>Fisujärvi</p>
-                            </div>
-                            <div class="middle-footer">
-                                <p>
-                                <div class ="CopyRight_footer">
-                                    <br>Copyright &copy; Fisujärvi 2024 - 2025<br>
+                            <img class="Avatar" src="fisu_avatar.png">
+                        </div>
+                        <div>
+                            <footer>
+                                <div class="top-footer">
+                                    <p>Fisujärvi</p>
                                 </div>
-                                </p>
-                            </div>
-                            <div class="social_icons_footer">
-                                <div class="icon"><i class="fa-brands fa-x-twitter"></i></div>
-                                <div class="icon"><i class="fa-brands fa-twitch"></i></div>
-                                <div class="icon"><i class="fa-brands fa-youtube"></i></div>
-                                <div class="icon"><i class="fa-brands fa-github"></i></div>
-                            </div>
-                        </footer>
+                                <div class="middle-footer">
+                                    <p>
+                                    <div class ="CopyRight_footer">
+                                        <br>Copyright &copy; Fisujärvi 2024 - 2025<br>
+                                    </div>
+                                    </p>
+                                </div>
+                                <div class="social_icons_footer">
+                                    <div class="icon"><i class="fa-brands fa-x-twitter"></i></div>
+                                    <div class="icon"><i class="fa-brands fa-twitch"></i></div>
+                                    <div class="icon"><i class="fa-brands fa-youtube"></i></div>
+                                    <div class="icon"><i class="fa-brands fa-github"></i></div>
+                                </div>
+                            </footer>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script>
-        const fisuTextarea = document.getElementById('dynamicFisuTextarea');
+        <script>
+            const fisuTextarea = document.getElementById('dynamicFisuTextarea');
 
-        fisuTextarea.addEventListener('input', function() {
-            this.style.height = 'auto';
-            this.style.height = this.scrollHeight + 'px';
-        });
-    </script>
+            fisuTextarea.addEventListener('input', function() {
+                this.style.height = 'auto';
+                this.style.height = this.scrollHeight + 'px';
+            });
+        </script>
 </x-app-layout>
--- public_html/Projekti/resources/views/layouts/navigation.blade.php
+++ public_html/Projekti/resources/views/layouts/navigation.blade.php
@@ -1,4 +1,3 @@
-
 <nav class="mainNav" x-data="{ open: false }"
     class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
